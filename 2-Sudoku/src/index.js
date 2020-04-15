e3cd809dbe76675a775be92ded65ef13d58541ea
Ross Nelson <RossN430@gmail.com>
2020-04-14 19:05:34 -0500

    begin Sudoku Add current tracker

--- 2-Sudoku/src/index.js
+++ 2-Sudoku/src/index.js
@@ -1,20 +1,23 @@
-import React from 'react';
+import React, { createElement } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { makepuzzle, solvepuzzle } from "sudoku";
 
-function Square(props) {
+const PUZZLE = makepuzzle();
+const SOLUTION = solvepuzzle(PUZZLE);
+
+function Cell(props) {
     return (
-        <button className="square" onClick={props.onClick}>
-            {props.value}
-        </button>
+        <input type="number" className="cell" min="0" max="8" maxLength="1" value={props.value}/*onClick={props.onClick}*/>
+        </input>
     );
 }
 
 class Board extends React.Component {
-    renderSquare(i) {
+    renderCell(i) {
         return (
-            <Square
-                value={this.props.squares[i]}
+            <Cell
+                value={this.props.cells[i]}
                 onClick={() => this.props.onClick(i)}
             />
         );
@@ -22,23 +25,107 @@ class Board extends React.Component {
 
     render() {
         return (
-            <div>
-                <div className="board-row">
-                    {this.renderSquare(0)}
-                    {this.renderSquare(1)}
-                    {this.renderSquare(2)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(3)}
-                    {this.renderSquare(4)}
-                    {this.renderSquare(5)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(6)}
-                    {this.renderSquare(7)}
-                    {this.renderSquare(8)}
-                </div>
-            </div>
+            <tbody className='board'>
+                <tr className='board-row'>
+                    <td>{this.renderCell(0)}</td>
+                    <td>{this.renderCell(1)}</td>
+                    <td>{this.renderCell(2)}</td>
+                    <td>{this.renderCell(3)}</td>
+                    <td>{this.renderCell(4)}</td>
+                    <td>{this.renderCell(5)}</td>
+                    <td>{this.renderCell(6)}</td>
+                    <td>{this.renderCell(7)}</td>
+                    <td>{this.renderCell(8)}</td>
+                </tr>
+                <tr className='board-row'>
+                    <td>{this.renderCell(9)}</td>
+                    <td>{this.renderCell(10)}</td>
+                    <td>{this.renderCell(11)}</td>
+                    <td>{this.renderCell(12)}</td>
+                    <td>{this.renderCell(13)}</td>
+                    <td>{this.renderCell(14)}</td>
+                    <td>{this.renderCell(15)}</td>
+                    <td>{this.renderCell(16)}</td>
+                    <td>{this.renderCell(17)}</td>
+                </tr>
+                <tr className='board-row'>
+                    <td>{this.renderCell(18)}</td>
+                    <td>{this.renderCell(19)}</td>
+                    <td>{this.renderCell(20)}</td>
+                    <td>{this.renderCell(21)}</td>
+                    <td>{this.renderCell(22)}</td>
+                    <td>{this.renderCell(23)}</td>
+                    <td>{this.renderCell(24)}</td>
+                    <td>{this.renderCell(25)}</td>
+                    <td>{this.renderCell(26)}</td>
+                </tr>
+                <tr className='board-row'>
+                    <td>{this.renderCell(27)}</td>
+                    <td>{this.renderCell(28)}</td>
+                    <td>{this.renderCell(29)}</td>
+                    <td>{this.renderCell(30)}</td>
+                    <td>{this.renderCell(31)}</td>
+                    <td>{this.renderCell(32)}</td>
+                    <td>{this.renderCell(33)}</td>
+                    <td>{this.renderCell(34)}</td>
+                    <td>{this.renderCell(35)}</td>
+                </tr>
+                <tr className='board-row'>
+                    <td>{this.renderCell(36)}</td>
+                    <td>{this.renderCell(37)}</td>
+                    <td>{this.renderCell(38)}</td>
+                    <td>{this.renderCell(39)}</td>
+                    <td>{this.renderCell(40)}</td>
+                    <td>{this.renderCell(41)}</td>
+                    <td>{this.renderCell(42)}</td>
+                    <td>{this.renderCell(43)}</td>
+                    <td>{this.renderCell(44)}</td>
+                </tr>
+                <tr className='board-row'>
+                    <td>{this.renderCell(45)}</td>
+                    <td>{this.renderCell(46)}</td>
+                    <td>{this.renderCell(47)}</td>
+                    <td>{this.renderCell(48)}</td>
+                    <td>{this.renderCell(49)}</td>
+                    <td>{this.renderCell(50)}</td>
+                    <td>{this.renderCell(51)}</td>
+                    <td>{this.renderCell(52)}</td>
+                    <td>{this.renderCell(53)}</td>
+                </tr>
+                <tr className='board-row'>
+                    <td>{this.renderCell(54)}</td>
+                    <td>{this.renderCell(55)}</td>
+                    <td>{this.renderCell(56)}</td>
+                    <td>{this.renderCell(57)}</td>
+                    <td>{this.renderCell(58)}</td>
+                    <td>{this.renderCell(59)}</td>
+                    <td>{this.renderCell(60)}</td>
+                    <td>{this.renderCell(61)}</td>
+                    <td>{this.renderCell(62)}</td>
+                </tr>
+                <tr className='board-row'>
+                    <td>{this.renderCell(63)}</td>
+                    <td>{this.renderCell(64)}</td>
+                    <td>{this.renderCell(65)}</td>
+                    <td>{this.renderCell(66)}</td>
+                    <td>{this.renderCell(67)}</td>
+                    <td>{this.renderCell(68)}</td>
+                    <td>{this.renderCell(69)}</td>
+                    <td>{this.renderCell(70)}</td>
+                    <td>{this.renderCell(71)}</td>
+                </tr>
+                <tr className='board-row'>
+                    <td>{this.renderCell(72)}</td>
+                    <td>{this.renderCell(73)}</td>
+                    <td>{this.renderCell(74)}</td>
+                    <td>{this.renderCell(75)}</td>
+                    <td>{this.renderCell(76)}</td>
+                    <td>{this.renderCell(77)}</td>
+                    <td>{this.renderCell(78)}</td>
+                    <td>{this.renderCell(79)}</td>
+                    <td>{this.renderCell(80)}</td>
+                </tr>
+            </tbody>
         );
     }
 }
@@ -48,70 +135,94 @@ class Game extends React.Component {
         super(props);
         this.state = {
             history: [{
-                squares: Array(9).fill(null),
+                cells: PUZZLE,
             }],
             stepNumber: 0,
-            xIsNext: true,
         };
     }
 
     handleClick(i) {
         const history = this.state.history.slice(0, this.state.stepNumber + 1);
         const current = history[history.length - 1];
-        const squares = current.squares.slice();
-        if (calculateWinner(squares) || squares[i]) {
+        const cells = current.cells.slice();
+        if (calculateWinner(cells) || cells[i]) {
             return;
         }
-        squares[i] = this.state.xIsNext ? "X" : "O";
+        cells[i] = i;
         this.setState({
             history: history.concat([
                 {
-                    squares: squares
+                    cells: cells
                 }
             ]),
             stepNumber: history.length,
-            xIsNext: !this.state.xIsNext
         });
+    }
+
+    solveMost() {
+        const history = this.state.history.slice(0, this.state.stepNumber + 1);
+        const current = history[history.length - 1];
+        const cells = current.cells.slice();
+        const solution = solvepuzzle(history[0].cells);
+
+        cells[0] = null;
+        for (let i = 1; i < solution.length; i++) {
+            console.log(solution.length);
+            cells[i] = solution[i];
+        }
+        this.setState({
+            history: history.concat([
+                {
+                    cells: cells
+                }
+            ]),
+            stepNumber: history.length,
+        })
     }
 
     jumpTo(step) {
         this.setState({
             stepNumber: step,
-            xIsNext: (step % 2) === 0
         });
     }
 
     render() {
         const history = this.state.history;
-        const current = history[this.state.stepNumber];
-        const winner = calculateWinner(current.squares);
+        const currentMove = this.state.stepNumber;
+        const current = history[currentMove];
+        const winner = calculateWinner(current.cells);
 
         const moves = history.map((step, move) => {
             const desc = move ?
                 'Go to move #' + move :
                 'Go to game start';
+            const cur = currentMove == move ?
+                ' (current)' :
+                '';
             return (
                 <li key={move}>
-                    <button onClick={() => this.jumpTo(move)}>{desc}</button>
+                    <button onClick={() => this.jumpTo(move)}>{desc}{cur}</button>
                 </li>
             );
         });
 
         let status;
         if (winner) {
-            status = 'Winner: ' + winner;
+            status = winner;
         } else {
-            status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+            status = <div><button className="solve" onClick={() => this.solveMost()}>Solve most of the Puzzle
+                          </button><br></br>Not Solved...
+                    </div>
         }
 
         return (
             <div className="game">
-                <div className="game-board">
+                <table className="game-board">
                     <Board
-                        squares={current.squares}
+                        cells={current.cells}
                         onClick={(i) => this.handleClick(i)}
                     />
-                </div>
+                </table>
                 <div className="game-info">
                     <div>{status}</div>
                     <ol>{moves}</ol>
@@ -128,22 +239,11 @@ ReactDOM.render(
     document.getElementById('root')
 );
 
-function calculateWinner(squares) {
-    const lines = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6],
-    ];
-    for (let i = 0; i < lines.length; i++) {
-        const [a, b, c] = lines[i];
-        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-            return squares[a];
+function calculateWinner(cells) {
+    for (let i = 0; i < cells.length; i++) {
+        if (cells[i] !== SOLUTION[i]) {
+            return null;
         }
     }
-    return null;
+    return "You win!";
 }

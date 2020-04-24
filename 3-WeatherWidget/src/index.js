2a46fc0669367b3e676c8821ef7984dec146334d
Ross Nelson <RossN430@gmail.com>
2020-04-24 17:27:39 -0500

    Experimenting with fetching json data

--- 3-WeatherWidget/src/index.js
+++ 3-WeatherWidget/src/index.js
@@ -1,64 +1,80 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import moment from 'moment'
 import './index.css';
 import { APIKEY } from './key.js'
 
 let ZIP = '60654'
-let url = 'https://api.openweathermap.org/data/2.5/forecast?zip='
+const URL = 'https://api.openweathermap.org/data/2.5/forecast?zip='
     + ZIP + '&APPID=' + APIKEY;
 
-fetch(url).then(function (response) {
-    response.json().then(function (json) {
-        console.log(json);
-    });
-});
-
 class Card extends React.Component {
-    constructor(props) {
-        super(props);
-    }
+    getHeader(json) {
+        let date = moment().format('dddd, MMMM Do YYYY, h:mm:ss a');
+        //let icon = this.json.weather.icon;
 
-    getHeader() {
+        console.log(json);
+
         return (
-            <div className="header">
-                HEADER
-            </div>
+            <section className="header">
+
+                Weather
+                {date}
+                REFRESH BUTTON
+            </section>
         );
     }
 
     getContent() {
         return (
-            <div className="content">
+            <section className="content">
                 CONTENT
-            </div>
+            </section>
         );
     }
 
     getActions() {
         return (
-            <div className="actions">
+            <section className="actions">
                 ACTIONS
-            </div>
+            </section>
         );
     }
 
     render() {
-        let header = this.getHeader();
+        const json = getWeather("Called from here");
+        console.log(json);
+
+        let header = this.getHeader(this.props.json);
         let content = this.getContent();
         let actions = this.getActions();
 
-
         return (
-            <div className="card">
+            <section className="card">
                 {header}
                 {content}
                 {actions}
-            </div>
+            </section>
         );
     }
 }
 
 ReactDOM.render(
-    <Card />,
+    <Card
+        json={getWeather("Called from card")}
+    />,
     document.getElementById('root')
 );
+
+function getWeather(message) {
+    console.log(message);
+    let jsondata;
+    fetch(URL).then(
+        function (u) { return u.json(); }
+    ).then(
+        function (json) {
+            jsondata = json;
+        }
+    )
+    return jsondata;
+}

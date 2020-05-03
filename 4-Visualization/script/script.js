d015bc2dc4ca3d29622577ca5ecedeb18574ff01
Ross Nelson <RossN430@gmail.com>
2020-05-02 21:58:14 -0500

    Complete

--- 4-Visualization/script/script.js
+++ 4-Visualization/script/script.js
@@ -2,6 +2,8 @@
 /* Ross Nelson Assignment 4: Visualization */
 /* May 5th, 2020 */
 
+let ALL = [[], [], [], [], []];
+
 function fetchData(URL) {
     return fetch(URL)
         .then((response) => {
@@ -12,97 +14,86 @@ function fetchData(URL) {
 }
 
 function getData(URL) {
-    let years = []
-    let total = []
-    let rail = []
-    let bus = []
-    let paratransit = []
-
-    fetchData(URL)
+    return fetchData(URL)
         .then((json) => {
             console.log(json)
             json.map(obj => {
-                years.push(obj.year);
-                total.push(obj.total);
-                rail.push(obj.rail);
-                bus.push(obj.bus);
-                paratransit.push(obj.paratransit);
+                ALL[0].push(obj.year);
+                ALL[1].push(obj.total);
+                ALL[2].push(obj.rail);
+                ALL[3].push(obj.bus);
+                ALL[4].push(obj.paratransit);
             })
+            return;
         })
         .catch(err => {
-            years = ["Could not retrieve"];
-            total = [0];
-            rail = [0];
-            bus = [0];
-            paratransit = [0];
+            ALL[0] = ["Could not retrieve"];
+            ALL[1] = [0];
+            ALL[2] = [0];
+            ALL[3] = [0];
+            ALL[4] = [0];
+            return;
         });
-
-    return [years, total, rail, bus, paratransit];
 }
 
-function getYears() {
-    let ALL = getData('https://data.cityofchicago.org/resource/w8km-9pzd');
-    console.log(ALL);
-    console.log(ALL[0]);
-    console.log(ALL[0][0]);
-    return ["January", "February", "March", "April", "May", "June", "July"];
-}
+getData('https://data.cityofchicago.org/resource/w8km-9pzd')
+    .then(() => {
+        let ctx = document.getElementById('myChart').getContext('2d');
+        let chart = new Chart(ctx, {
+            // The type of chart we want to create
+            type: 'bar',
 
-let ctx = document.getElementById('myChart').getContext('2d');
-let chart = new Chart(ctx, {
-    // The type of chart we want to create
-    type: 'bar',
-
-    // The data for our dataset
-    data: {
-        labels: getYears(),
-        datasets: [{
-            label: 'Total',
-            backgroundColor: 'rgb(147, 243, 169)',
-            borderColor: 'rgb(87, 237, 120)',
-            data: [0, 10, 5, 2, 500000, 30, 45]
-        },
-        {
-            label: 'Rail',
-            backgroundColor: 'rgb(242, 145, 150)',
-            borderColor: 'rgb(235, 85, 92)',
-            data: [10, 5, 2, 20, 30, 45, 0]
-        },
-        {
-            label: 'Bus',
-            backgroundColor: 'rgb(151, 145, 240)',
-            borderColor: 'rgb(98, 88, 232)',
-            data: [10, 5, 2, 20, 30, 45, 0]
-        },
-        {
-            label: 'Paratransit',
-            backgroundColor: 'rgb(252, 212, 148)',
-            borderColor: 'rgb(249, 174, 52)',
-            data: [10, 5, 2, 20, 30, 45, 0]
-        }]
-    },
-
-    // Configuration options go here
-    options: {
-        scales: {
-            xAxes: [{
-                scaleLabel: {
-                    display: true,
-                    labelString: 'Year'
-                }
-            }],
-            yAxes: [{
-                scaleLabel: {
-                    display: true,
-                    labelString: 'Boardings',
+            // The data for our dataset
+            data: {
+                labels: ALL[0],
+                datasets: [{
+                    label: 'Total',
+                    backgroundColor: 'rgb(147, 243, 169)',
+                    borderColor: 'rgb(87, 237, 120)',
+                    data: ALL[1]
                 },
-                type: 'logarithmic',
-                ticks: {
-                    callback: function (value, index, values) {
-                        return Number(value.toString());
-                    }
+                {
+                    label: 'Rail',
+                    backgroundColor: 'rgb(242, 145, 150)',
+                    borderColor: 'rgb(235, 85, 92)',
+                    data: ALL[2]
+                },
+                {
+                    label: 'Bus',
+                    backgroundColor: 'rgb(151, 145, 240)',
+                    borderColor: 'rgb(98, 88, 232)',
+                    data: ALL[3]
+                },
+                {
+                    label: 'Paratransit',
+                    backgroundColor: 'rgb(252, 212, 148)',
+                    borderColor: 'rgb(249, 174, 52)',
+                    data: ALL[4]
+                }]
+            },
+
+            // Configuration options go here
+            options: {
+                scales: {
+                    xAxes: [{
+                        scaleLabel: {
+                            display: true,
+                            labelString: 'Year'
+                        }
+                    }],
+                    yAxes: [{
+                        scaleLabel: {
+                            display: true,
+                            labelString: 'Boardings',
+                        },
+                        type: 'logarithmic',
+                        ticks: {
+                            callback: function (value, index, values) {
+                                return Number(value.toString());
+                            }
+                        }
+                    }]
                 }
-            }]
-        }
-    }
-});
+            }
+        });
+    });
